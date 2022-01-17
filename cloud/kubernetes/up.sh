3d1c239049af46cbdfb30a3710d099fd5f9712c2
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-01-16 16:48:47 -0800

    vpc

--- cloud/kubernetes/up.sh
+++ cloud/kubernetes/up.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 export AWS_REGION='us-west-2'
-export CLUSTER_NAME='test-cluster'
-export VPC_ID="vpc-0f3dce67ace642302"
+export CLUSTER_NAME='cluster-WC'
+export VPC_ID="vpc-063c7828f9133d744"
 
 # Create Cluster
 eksctl create cluster --name=$CLUSTER_NAME --region=$AWS_REGION --fargate \
-  --vpc-private-subnets=subnet-0c001b34bfe93a0eb,subnet-092f5c56c0f24ce34
+  --vpc-private-subnets=subnet-047ac0ee28dcda782,subnet-0e9929d35b9179750
 
 # Approve cluster to associate IAM OpenID Connect Provider
 eksctl utils associate-iam-oidc-provider --cluster=$CLUSTER_NAME --approve
