cd6c1940af52b872a976355302b8c9d263f5a79f
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-01-17 18:31:51 -0800

    kubernetes

---
+++ cloud/kubernetes/up.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+export AWS_REGION='us-west-2'
+export CLUSTER_NAME='cluster-WC'
+export VPC_ID=vpc-0a93b6feb1baef3f1
+
+# Create Cluster
+# eksctl create cluster --name=$CLUSTER_NAME --region=$AWS_REGION --fargate \
+#   --vpc-private-subnets=subnet-0be379bfce314d616,subnet-014053119aaa60ab3
+
+# Approve cluster to associate IAM OpenID Connect Provider
+# eksctl utils associate-iam-oidc-provider --cluster=$CLUSTER_NAME --approve
+
+# Create IAM Policy
+#curl -O "https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/iam-policy.json"
+#aws iam create-policy --policy-name ALBIngressControllerIAMPolicy --policy-document file://iam-policy.json
+#rm iam-policy.json
+
+export STACK_NAME=eksctl-$CLUSTER_NAME-cluster
+export AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
+
+# Setup ALB Ingress Controller
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/rbac-role.yaml
+
+eksctl create iamserviceaccount \
+       --name=alb-ingress-controller \
+       --namespace=kube-system \
+       --cluster=$CLUSTER_NAME \
+       --attach-policy-arn=arn:aws:iam::$AWS_ACCOUNT_ID:policy/ALBIngressControllerIAMPolicy \
+       --override-existing-serviceaccounts \
+       --approve
+
+curl -sS "https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/alb-ingress-controller.yaml" \
+     | sed "s/# - --cluster-name=devCluster/- --cluster-name=$CLUSTER_NAME/g" \
+     | sed "s/# - --aws-vpc-id=vpc-xxxxxx/- --aws-vpc-id=$VPC_ID/g" \
+     | sed "s/# - --aws-region=us-west-1/- --aws-region=$AWS_REGION/g" \
+     | kubectl apply -f -
+
+# Import Secrets
+kubectl create secret generic db-info \
+  --from-file=db_user=./secrets/db_user.txt \
+  --from-file=db_host=./secrets/db_host.txt \
+  --from-file=db_user_password=./secrets/db_user_password.txt
+kubectl create secret generic jwt-secret \
+  --from-file=secret_key=./secrets/secret_key.txt
+
+# Apply config files
+kubectl apply -f service.yaml -f ingress.yaml -f cloudwatch.yaml
+
+sed -e 's/$AWS_REGION/'"$AWS_REGION"'/g' -e 's/$AWS_ACCOUNT_ID/'"$AWS_ACCOUNT_ID"'/g' deployment.yaml | kubectl apply -f -
