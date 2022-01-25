58624261935e036ada236a0984c8493d6fc475ec
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-01-24 18:55:44 -0800

    get .env from Jenkinsfile instead of .env

--- cloud/kubernetes/down.sh
+++ cloud/kubernetes/down.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-export CLUSTER_NAME=$CLUSTER_NAME
-export AWS_REGION=$AWS_REGION
-
 aws eks --region $AWS_REGION update-kubeconfig --name $CLUSTER_NAME
 kubectl delete ingress utopia-ingress
 eksctl delete cluster $CLUSTER_NAME --region $AWS_REGION
--- cloud/kubernetes/up.sh
+++ cloud/kubernetes/up.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-export AWS_REGION=$AWS_REGION
-export CLUSTER_NAME=$CLUSTER_NAME
+
 export VPC_ID=$(aws ec2 describe-vpcs --filter Name=tag:Name,Values=WC-vpc | jq '.[] | .[].VpcId' | tr -d '"') 
 PRIVATE_SUBNETS=$(aws ec2 describe-subnets --filter Name=vpc-id,Values=$VPC_ID --query 'Subnets[?MapPublicIpOnLaunch==`false`].SubnetId')
 export PRIVATE_SUBNETS
 
 export private_subnet1=$(echo $PRIVATE_SUBNETS | jq '.[0]')
 export private_subnet2=$(echo $PRIVATE_SUBNETS | jq '.[1]')
-
-echo $DB_HOST
 
 
 # Create Cluster
@@ -58,12 +55,3 @@ kubectl apply -f service.yaml -f ingress.yaml -f cloudwatch.yaml
 
 sed -e 's/$AWS_REGION/'"$AWS_REGION"'/g' -e 's/$AWS_ACCOUNT_ID/'"$AWS_ACCOUNT_ID"'/g' deployment.yaml | kubectl apply -f -
 
-
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/main/deploy/rbac-secretproviderclass.yaml
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/main/deploy/csidriver.yaml
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/main/deploy/secrets-store.csi.x-k8s.io_secretproviderclasses.yaml
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/main/deploy/secrets-store.csi.x-k8s.io_secretproviderclasspodstatuses.yaml
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/main/deploy/secrets-store-csi-driver.yaml
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/main/deploy/rbac-secretprovidersyncing.yaml
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/main/deploy/rbac-secretproviderrotation.yaml
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/main/deploy/secrets-store-csi-driver-windows.yaml
