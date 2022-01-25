1f20be534074771705514c1366a0fd0f45497b8e
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-01-24 18:36:27 -0800

    secrets from .env

--- cloud/kubernetes/up.sh
+++ cloud/kubernetes/up.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
-export AWS_REGION='us-west-2'
-export CLUSTER_NAME='cluster-WC'
-export VPC_ID=$(aws ec2 describe-vpcs --filter Name=tag:Name,Values=WC-vpc | jq '.[] | .[].VpcId')
+# export AWS_REGION='us-west-2'
+# export CLUSTER_NAME='wc'
+export VPC_ID=$(aws ec2 describe-vpcs --filter Name=tag:Name,Values=WC-vpc | jq '.[] | .[].VpcId' | tr -d '"') 
+PRIVATE_SUBNETS=$(aws ec2 describe-subnets --filter Name=vpc-id,Values=$VPC_ID --query 'Subnets[?MapPublicIpOnLaunch==`false`].SubnetId')
+export PRIVATE_SUBNETS
 
-SUBNETS=$(aws ec2 describe-subnets --filter Name=vpc-id,Values=$VPC_ID --query 'Subnets[?MapPublicIpOnLaunch==`false`].SubnetId')
-export SUBNETS
-export subnet1=$(echo $SUBNETS | jq '.[0]')
-export subnet2=$(echo $SUBNETS | jq '.[1]')
+export private_subnet1=$(echo $PRIVATE_SUBNETS | jq '.[0]')
+export private_subnet2=$(echo $PRIVATE_SUBNETS | jq '.[1]')
+
+echo $DB_HOST
+
+
 # Create Cluster
 eksctl create cluster --name=$CLUSTER_NAME --region=$AWS_REGION --fargate \
-  --vpc-private-subnets=$subnet1,$subnet2
+  --vpc-private-subnets=$private_subnet1,$private_subnet2
 
 # Approve cluster to associate IAM OpenID Connect Provider
-# eksctl utils associate-iam-oidc-provider --cluster=$CLUSTER_NAME --approve
+eksctl utils associate-iam-oidc-provider --cluster=$CLUSTER_NAME --approve
 
 # Create IAM Policy
 #curl -O "https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/iam-policy.json"
@@ -34,6 +38,7 @@ eksctl create iamserviceaccount \
        --override-existing-serviceaccounts \
        --approve
 
+
 curl -sS "https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/alb-ingress-controller.yaml" \
      | sed "s/# - --cluster-name=devCluster/- --cluster-name=$CLUSTER_NAME/g" \
      | sed "s/# - --aws-vpc-id=vpc-xxxxxx/- --aws-vpc-id=$VPC_ID/g" \
@@ -42,13 +47,23 @@ curl -sS "https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-cont
 
 # Import Secrets
 kubectl create secret generic db-info \
-  --from-file=db_user=./secrets/db_user.txt \
-  --from-file=db_host=./secrets/db_host.txt \
-  --from-file=db_user_password=./secrets/db_user_password.txt
+  --from-literal=db_user=$DB_USER \
+  --from-literal=db_host=$DB_HOST \
+  --from-literal=db_user_password=$DB_USER_PASSWORD
 kubectl create secret generic jwt-secret \
-  --from-file=secret_key=./secrets/secret_key.txt
+  --from-literal=secret_key=$SECRET_KEY
 
 # Apply config files
-kubectl apply -f service.yaml -f ingress.yaml -f namespace.yaml -f cloudwatch.yaml
+kubectl apply -f service.yaml -f ingress.yaml -f cloudwatch.yaml
 
 sed -e 's/$AWS_REGION/'"$AWS_REGION"'/g' -e 's/$AWS_ACCOUNT_ID/'"$AWS_ACCOUNT_ID"'/g' deployment.yaml | kubectl apply -f -
+
+
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/main/deploy/rbac-secretproviderclass.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/main/deploy/csidriver.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/main/deploy/secrets-store.csi.x-k8s.io_secretproviderclasses.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/main/deploy/secrets-store.csi.x-k8s.io_secretproviderclasspodstatuses.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/main/deploy/secrets-store-csi-driver.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/main/deploy/rbac-secretprovidersyncing.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/main/deploy/rbac-secretproviderrotation.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/main/deploy/secrets-store-csi-driver-windows.yaml
