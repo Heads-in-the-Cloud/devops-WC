5c4764dc35313ed4fcc74cb2b781cd6928158059
walterchangsmoothstack <90656351+walterchangsmoothstack@users.noreply.github.com>
2022-02-04 13:22:12 -0800

    Delete cloud directory

--- cloud/cloudformation/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CF_STACK_WC='wc-utopia-cf'
-cluster_name=$(aws ecs list-clusters | grep ${CF_STACK_WC})
-echo $cluster_name
-service_name=$(aws ecs list-services --cluster ${cluster_name} | grep UsersService)
-# service_name=$(aws ecs list-services --cluster "arn:aws:ecs:us-west-2:026390315914:cluster/wc-utopia-cf-ECSCluster-TLZBxOpt0ywg" | tr -d '"' | grep UsersService)
-echo $service_name
-echo $cluster_name
-# aws ecs update-service --cluster ${cluster_name} --service ${service_name} --task-definition users-task-WC --force-new-deployment
