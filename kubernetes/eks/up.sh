a8379021ee5b575803247ad12a0c830537094d85
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-02-09 12:30:18 -0800

    UTOPIA-341 change back to using Jenkinsfile stages

--- kubernetes/eks/up.sh
+++ kubernetes/eks/up.sh
@@ -1,46 +1,46 @@
 #!/bin/sh
 
 
-export VPC_ID=$(aws ec2 describe-vpcs --filter Name=tag:Name,Values=WC-vpc | jq '.[] | .[].VpcId' | tr -d '"') 
-PRIVATE_SUBNETS=$(aws ec2 describe-subnets --filter Name=vpc-id,Values=$VPC_ID --query 'Subnets[?MapPublicIpOnLaunch==`false`].SubnetId')
-export PRIVATE_SUBNETS
+# export VPC_ID=$(aws ec2 describe-vpcs --filter Name=tag:Name,Values=WC-vpc | jq '.[] | .[].VpcId' | tr -d '"') 
+# PRIVATE_SUBNETS=$(aws ec2 describe-subnets --filter Name=vpc-id,Values=$VPC_ID --query 'Subnets[?MapPublicIpOnLaunch==`false`].SubnetId')
+# export PRIVATE_SUBNETS
 
-export private_subnet1=$(echo $PRIVATE_SUBNETS | jq '.[0]')
-export private_subnet2=$(echo $PRIVATE_SUBNETS | jq '.[1]')
-
-
-# Create Cluster
-eksctl create cluster --name=$CLUSTER_NAME --region=$AWS_REGION --fargate \
-  --vpc-private-subnets=$private_subnet1,$private_subnet2
-
-# Approve cluster to associate IAM OpenID Connect Provider
-eksctl utils associate-iam-oidc-provider --cluster=$CLUSTER_NAME --approve
-
-# Create IAM Policy
-#curl -O "https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/iam-policy.json"
-#aws iam create-policy --policy-name ALBIngressControllerIAMPolicy --policy-document file://iam-policy.json
-#rm iam-policy.json
-
-export STACK_NAME=eksctl-$CLUSTER_NAME-cluster
-export AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID
-
-# Setup ALB Ingress Controller
-kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/rbac-role.yaml
-
-eksctl create iamserviceaccount \
-       --name=alb-ingress-controller \
-       --namespace=kube-system \
-       --cluster=$CLUSTER_NAME \
-       --attach-policy-arn=arn:aws:iam::$AWS_ACCOUNT_ID:policy/ALBIngressControllerIAMPolicy \
-       --override-existing-serviceaccounts \
-       --approve
+# export private_subnet1=$(echo $PRIVATE_SUBNETS | jq '.[0]')
+# export private_subnet2=$(echo $PRIVATE_SUBNETS | jq '.[1]')
 
 
-curl -sS "https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/alb-ingress-controller.yaml" \
-     | sed "s/# - --cluster-name=devCluster/- --cluster-name=$CLUSTER_NAME/g" \
-     | sed "s/# - --aws-vpc-id=vpc-xxxxxx/- --aws-vpc-id=$VPC_ID/g" \
-     | sed "s/# - --aws-region=us-west-1/- --aws-region=$AWS_REGION/g" \
-     | kubectl apply -f -
+# # Create Cluster
+# eksctl create cluster --name=$CLUSTER_NAME --region=$AWS_REGION --fargate \
+#   --vpc-private-subnets=$private_subnet1,$private_subnet2
+
+# # Approve cluster to associate IAM OpenID Connect Provider
+# eksctl utils associate-iam-oidc-provider --cluster=$CLUSTER_NAME --approve
+
+# # Create IAM Policy
+# #curl -O "https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/iam-policy.json"
+# #aws iam create-policy --policy-name ALBIngressControllerIAMPolicy --policy-document file://iam-policy.json
+# #rm iam-policy.json
+
+# export STACK_NAME=eksctl-$CLUSTER_NAME-cluster
+# export AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID
+
+# # Setup ALB Ingress Controller
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/rbac-role.yaml
+
+# eksctl create iamserviceaccount \
+#        --name=alb-ingress-controller \
+#        --namespace=kube-system \
+#        --cluster=$CLUSTER_NAME \
+#        --attach-policy-arn=arn:aws:iam::$AWS_ACCOUNT_ID:policy/ALBIngressControllerIAMPolicy \
+#        --override-existing-serviceaccounts \
+#        --approve
+
+
+# curl -sS "https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/alb-ingress-controller.yaml" \
+#      | sed "s/# - --cluster-name=devCluster/- --cluster-name=$CLUSTER_NAME/g" \
+#      | sed "s/# - --aws-vpc-id=vpc-xxxxxx/- --aws-vpc-id=$VPC_ID/g" \
+#      | sed "s/# - --aws-region=us-west-1/- --aws-region=$AWS_REGION/g" \
+#      | kubectl apply -f -
 
 # Import Secrets
 kubectl create secret generic db-info \
