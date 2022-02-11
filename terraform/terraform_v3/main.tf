8b395bbc6ab4c422e1cfa155da8619d7b4267474
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-02-11 15:38:21 -0800

    UTOPIA-363 test tagging subnets

--- terraform/terraform_v3/main.tf
+++ terraform/terraform_v3/main.tf
@@ -75,10 +75,10 @@ module "networks" {
   vpc_id                = data.aws_vpc.jenkins_vpc.id
   region                = "us-west-2"
   cluster_name          = "UtopiaClusterWC"
-  public_subnet_1       = data.jenkins_public_1.id
-  public_subnet_2       = data.jenkins_public_2.id
-  private_subnet_1      = data.jenkins_private_1.id
-  private_subnet_2      = data.jenkins_private_2.id
+  public_subnet_1       = data.aws_subnet.jenkins_public_1.id
+  public_subnet_2       = data.aws_subnet.jenkins_public_2.id
+  private_subnet_1      = data.aws_subnet.jenkins_private_1.id
+  private_subnet_2      = data.aws_subnet.jenkins_private_2.id
   rt_id                 = data.aws_route_table.private_rt.id
 }
 
