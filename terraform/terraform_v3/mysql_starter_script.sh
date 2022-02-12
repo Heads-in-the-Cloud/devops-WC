ca6e0d3918e24e5fc5b306c062024634b01afb6a
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-02-11 16:53:06 -0800

    UTOPIA-363 terminate EC2 after userdata is run

--- terraform/terraform_v3/mysql_starter_script.sh
+++ terraform/terraform_v3/mysql_starter_script.sh
@@ -6,3 +6,5 @@ yum install mysql -y
 aws s3 cp s3://utopia-bucket-wc/tinydump.sql .
 
 mysql -h ${RDS_MYSQL_ENDPOINT} -u ${RDS_MYSQL_USER} -p${RDS_MYSQL_PASS} -D ${RDS_MYSQL_BASE} < tinydump.sql
+
+sudo shutdown now -h
