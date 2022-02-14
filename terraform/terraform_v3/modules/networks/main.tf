ae0e09f26bf8b18148775c92db9ac44734798998
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-02-14 11:56:00 -0800

    UTOPIA-363 adding environment selection

--- terraform/terraform_v3/modules/networks/main.tf
+++ terraform/terraform_v3/modules/networks/main.tf
@@ -1,22 +1,22 @@
 
-# resource "aws_eip" "nat" {
-#   vpc        = true
-# }
+resource "aws_eip" "nat" {
+  vpc        = true
+}
 
-# resource "aws_nat_gateway" "nat" {
-#   allocation_id = aws_eip.nat.id
-#   subnet_id     = var.public_subnet_1
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = var.public_subnet_1
 
-#   tags = {
-#     Name        = "Jenkins_nat"
-#   }
-# }
+  tags = {
+    Name        = "Jenkins_nat"
+  }
+}
 
-# resource "aws_route" "private_nat_gateway" {
-#   route_table_id         = var.rt_id
-#   destination_cidr_block = var.public_cidr_block
-#   nat_gateway_id         = aws_nat_gateway.nat.id
-# }
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = var.rt_id
+  destination_cidr_block = var.public_cidr_block
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
 
 resource "aws_db_subnet_group" "private-subnet-group" {
   name       = "wc_private_subnet_group"
