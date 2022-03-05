ae7bf6b8791bb36109f130ba39a0dfb57af45b71
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-03-04 19:32:47 -0800

    UTOPIA-363 added environment to cluster name

--- ecs/ecs-cli/down.sh
+++ ecs/ecs-cli/down.sh
@@ -15,15 +15,3 @@ rm temp.yaml
 rm temp-docker-compose.yaml
 
 ecs-cli compose --project-name ${1} --file docker-compose.yaml service rm
-
-# sed -e 's/$SERVICE/'"${FLIGHTS_CONTAINER}"'/g' compose-template.yaml > docker-compose.yaml
-
-# ecs-cli compose --project-name ${FLIGHTS_SERVICE} --file docker-compose.yaml service rm
-
-# sed -e 's/$SERVICE/'"${BOOKINGS_CONTAINER}"'/g' compose-template.yaml > docker-compose.yaml
-
-# ecs-cli compose --project-name ${BOOKINGS_SERVICE} --file docker-compose.yaml service rm
-
-# sed -e 's/$SERVICE/'"${FRONTEND_CONTAINER}"'/g' compose-template.yaml > docker-compose.yaml
-
-# ecs-cli compose --project-name ${FRONTEND_SERVICE} --file docker-compose.yaml service rm
