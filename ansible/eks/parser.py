3c383eeb4596413d2abb0599db3fd5871c141c22
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-03-08 00:07:01 -0800

    UTOPIA-363 parser.yaml to parse json

--- ansible/eks/parser.py
+++ ansible/eks/parser.py
@@ -8,7 +8,7 @@ import ast
 if __name__ == '__main__':
     print(sys.argv)
     print(type(sys.argv[1]))
-    dict_string = sys.argv[1]
+    dict_string = sys.argv[1][1:][:-1]
     object = dict_string.split(',')
     print(object)
     # object = ast.literal_eval(sys.argv[1])
