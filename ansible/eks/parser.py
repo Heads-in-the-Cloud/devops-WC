089c94fcef148683452a2cc8c6c8ab10a43d30d9
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-03-08 12:36:19 -0800

    UTOPIA-363 testing parse.py

--- ansible/eks/parser.py
+++ ansible/eks/parser.py
@@ -6,12 +6,11 @@ import ast
 
 
 if __name__ == '__main__':
-    print(sys.argv)
-    print(type(sys.argv[1]))
+
     dict_string = sys.argv[1][1:][:-1]
     object = dict_string.split(',')
     dict_obj = {}
     for x in object:
         parse_x = x.split(':')
         dict_obj[parse_x[0]] = parse_x[1]
-    print(dict_obj[sys.argv[2]])
+    sys.exit(dict_obj[sys.argv[1]])
