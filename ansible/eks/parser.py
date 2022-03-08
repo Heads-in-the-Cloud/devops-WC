84b93ccf91127944793367e5f91a8f24c14e7c2a
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-03-08 00:09:30 -0800

    UTOPIA-363 parser.yaml to parse json

--- ansible/eks/parser.py
+++ ansible/eks/parser.py
@@ -10,7 +10,11 @@ if __name__ == '__main__':
     print(type(sys.argv[1]))
     dict_string = sys.argv[1][1:][:-1]
     object = dict_string.split(',')
-    print(object)
+    dict_obj = {}
+    for x in object:
+        parse_x = x.split(':')
+        dict_obj[parse_x[0]] = parse_x[1]
+    print(dict_obj)
     # object = ast.literal_eval(sys.argv[1])
     # print(object)
     # print(object['secret_key'])
