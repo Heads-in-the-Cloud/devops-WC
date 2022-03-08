5cab4c6a45aca873d1e2dd88dd29fc5485290d14
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-03-07 23:52:23 -0800

    UTOPIA-363 parser.yaml to parse json

--- ansible/eks/parser.py
+++ ansible/eks/parser.py
@@ -2,9 +2,12 @@
 
 import json
 import sys
+import ast
 
 
 if __name__ == '__main__':
     print(sys.argv)
     print(type(sys.argv[1]))
-    json.load(sys.argv[1])
+    object = ast.literal_eval
+    print(object)
+    print(object['secret_key'])
