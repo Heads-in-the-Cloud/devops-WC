0ec085d0d0cd591a4eb0d27413336a0a91d5de44
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-03-09 20:09:56 -0800

    UTOPIA-363 remove unneccessary line

--- ecs/ecs-cli/down.sh
+++ ecs/ecs-cli/down.sh
@@ -7,8 +7,6 @@ sed -e 's/$SERVICE/'"${1}"':/g' compose-template.yaml > temp-docker-compose.yaml
 # Inject environment variables into definition file
 envsubst < "temp-docker-compose.yaml" > "docker-compose.yaml"
 
-cat docker-compose.yaml
-rm temp.yaml
 rm temp-docker-compose.yaml
 
 ecs-cli compose --project-name ${1} --file docker-compose.yaml service rm --timeout 10
