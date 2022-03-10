32057e50d1b1acf1ef7349f5853bd71753996a07
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-03-10 14:59:48 -0800

    UTOPIA-363 terraform_v2 test for cross-region deployment

--- terraform/terraform_v2/mysql_starter_script.sh
+++ terraform/terraform_v2/mysql_starter_script.sh
@@ -6,3 +6,14 @@ yum install mysql -y
 aws s3 cp s3://utopia-bucket-wc/tinydump.sql .
 
 mysql -h ${RDS_MYSQL_ENDPOINT} -u ${RDS_MYSQL_USER} -p${RDS_MYSQL_PASS} -D ${RDS_MYSQL_BASE} < tinydump.sql
+wait
+
+#shut down/terminate bastion host
+if [[ $ENVIRONMENT=='prod' ]]
+then
+    echo 'prod'
+    sudo shutdown now -h
+else
+    echo 'dev'
+    poweroff
+fi
