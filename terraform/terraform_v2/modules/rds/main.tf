12308da04717bc297ba7d5958a32ef5e005ed5d8
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-03-16 12:29:17 -0700

    UTOPIA-363 configure secrets outside of main.tf

--- terraform/terraform_v2/modules/rds/main.tf
+++ terraform/terraform_v2/modules/rds/main.tf
@@ -22,20 +22,20 @@ resource "aws_secretsmanager_secret_version" "secret_string" {
                                    {"db_user"     = var.db_user}))
 }
 
-# resource "aws_db_instance" "rds" {
-#   allocated_storage    = 10
-#   engine               = var.db_engine
-#   engine_version       = var.db_engine_version
-#   instance_class       = var.db_instance
-#   name                 = var.db_name
-#   username             = var.db_username
-#   password             = var.secrets_data["db_password"]
-#   skip_final_snapshot  = true
-#   identifier           = var.db_identifier
-#   db_subnet_group_name = var.subnet_group_id
-#   vpc_security_group_ids = [ aws_security_group.db_sg.id ]
+resource "aws_db_instance" "rds" {
+  allocated_storage    = 10
+  engine               = var.db_engine
+  engine_version       = var.db_engine_version
+  instance_class       = var.db_instance
+  name                 = var.db_name
+  username             = var.db_username
+  password             = var.secrets_data["db_password"]
+  skip_final_snapshot  = true
+  identifier           = var.db_identifier
+  db_subnet_group_name = var.subnet_group_id
+  vpc_security_group_ids = [ aws_security_group.db_sg.id ]
 
-# }
+}
 
 resource "aws_security_group_rule" "ingress_rules" {
   count = length(var.rds_ingress)
