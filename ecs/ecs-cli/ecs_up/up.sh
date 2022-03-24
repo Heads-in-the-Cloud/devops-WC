a400779a45a706bfb5e5183c94dd8a46c5334331
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-03-24 16:28:24 -0700

    UTOPIA-363 have Jenkins stages in docker-compose up run in parallel

--- ecs/ecs-cli/ecs_up/up.sh
+++ ecs/ecs-cli/ecs_up/up.sh
@@ -17,7 +17,6 @@ do
     IFS='=' read -ra var <<< ${ECS_VARS[$i]}
     temp=${var[1]////'\/'}
     temp=${temp//\'/}
-    echo $temp
     ARGS+="
      - ${var[0]}=$temp" # replace all slashes to appease SED
 
@@ -29,29 +28,29 @@ done
 
 #Use sed to insert environment variables metadata
 sed -e 's/$SERVICE/'"${1}:"' \
-    '"$ARGS"' /g' definition_files/compose-template.yaml > temp-docker-compose.yaml
+    '"$ARGS"' /g' definition_files/compose-template.yaml > ${1}-temp-docker-compose.yaml
 
 
 # Inject environment variables in definition files
-envsubst < "./definition_files/ecs-template.yaml" > "ecs-params.yaml"
-envsubst < "temp-docker-compose.yaml" > "docker-compose.yaml"
+envsubst < "./definition_files/ecs-template.yaml" > "${1}-ecs-params.yaml"
+envsubst < "${1}-temp-docker-compose.yaml" > "${1}-docker-compose.yaml"
 
 
 # Remove temporary files
-rm temp-docker-compose.yaml
+rm ${1}-temp-docker-compose.yaml
 
-cat docker-compose.yaml
-cat ecs-params.yaml
+cat "${1}"-docker-compose.yaml
+cat "${1}"-ecs-params.yaml
 
 
 #Docker-Compose Up with target-group 
-ecs-cli compose --file docker-compose.yaml --project-name ${1} \
---ecs-params ecs-params.yaml service up \
+ecs-cli compose --file ${1}-docker-compose.yaml --project-name ${1} \
+--ecs-params ${1}-ecs-params.yaml service up \
 --force-deployment \
 --target-groups "targetGroupArn=${3},containerName=${1},containerPort=${PORT}" 
 
 #Scale the service to the desired count
-ecs-cli compose --project-name ${1} --file docker-compose.yaml service scale ${DESIRED_COUNT}
+ecs-cli compose --project-name ${1} --file ${1}-docker-compose.yaml service scale ${DESIRED_COUNT}
 
 rm ecs-params.yaml
 rm docker-compose.yaml
