af5b02090ce884e17b370a51455aa5e3aba62ae6
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-03-28 17:06:01 -0700

    UTOPIA-363 configure secrets with rds terraform script

--- terraform/rds/variables.tf
+++ terraform/rds/variables.tf
@@ -53,88 +53,81 @@ variable "rds_ingress" {
         to_port         = number
         protocol        = string
         cidr_blocks     = list(string)
+    }))
+    default = [
+          {
+            description      = "Allow HTTP from any IPv4",
+            from_port        = 80,
+            to_port          = 80,
+            protocol         = "tcp",
+            cidr_blocks      = ["0.0.0.0/0"],
+          },
+          {
+            description      = "Allow connection to MYSQL",
+            from_port        = 3306,
+            to_port          = 3306,
+            protocol         = "tcp",
+            cidr_blocks      = ["0.0.0.0/0"],           
+          }]
+  }
+  variable "rds_egress" {
+    type = list(object({
+        description     = string
+        from_port       = number        
+        to_port         = number
+        protocol        = string
+        cidr_blocks     = list(string)
         ipv6_cidr_blocks= list(string)
     }))
     default = [
-                          {
-                            description      = "Allow HTTP from any IPv4",
-                            from_port        = 80,
-                            to_port          = 80,
-                            protocol         = "tcp",
-                            cidr_blocks      = ["0.0.0.0/0"],
-                          },
-                          {
-                            description      = "Allow connection to MYSQL",
-                            from_port        = 3306,
-                            to_port          = 3306,
-                            protocol         = "tcp",
-                            cidr_blocks      = ["0.0.0.0/0"],           
-                          }
-                          ]
-  rds_egress            = [{
-                            description      = "Allow egress to anywhere ipv4/ipv6",
-                            from_port        = 0,
-                            to_port          = 0,
-                            protocol         = "-1",
-                            cidr_blocks      = ["0.0.0.0/0"],
-                            ipv6_cidr_blocks = ["::/0"]
-                          }]
-  ec2_ingress           = [{
-                            description      = "Allow SSH from anywhere",
-                            from_port        = 22,
-                            to_port          = 22,
-                            protocol         = "tcp",
-                            cidr_blocks      = ["0.0.0.0/0"], 
-                          }]
-  ec2_egress            = [{
-                          description      = "Allow egress to anywhere ipv4/ipv6",
-                          from_port        = 0,
-                          to_port          = 0,
-                          protocol         = "-1",
-                          cidr_blocks      = ["0.0.0.0/0"],
-                          ipv6_cidr_blocks = ["::/0"]
-                          }]
+      {
+        description      = "Allow egress to anywhere ipv4/ipv6",
+        from_port        = 0,
+        to_port          = 0,
+        protocol         = "-1",
+        cidr_blocks      = ["0.0.0.0/0"],
+        ipv6_cidr_blocks = ["::/0"]
+      }]
+  }
+
+  variable "ec2_ingress"{
+    type = list(object({
+        description     = string
+        from_port       = number        
+        to_port         = number
+        protocol        = string
+        cidr_blocks     = list(string)
+    }))
+
+    default = [
+      {
+        description      = "Allow SSH from anywhere",
+        from_port        = 22,
+        to_port          = 22,
+        protocol         = "tcp",
+        cidr_blocks      = ["0.0.0.0/0"], 
+      }]
+  }
+  variable "ec2_egress" {
+    type = list(object({
+        description     = string
+        from_port       = number        
+        to_port         = number
+        protocol        = string
+        cidr_blocks     = list(string)
+        ipv6_cidr_blocks= list(string)
+    }))
+    default = [
+      {
+        description      = "Allow egress to anywhere ipv4/ipv6",
+        from_port        = 0,
+        to_port          = 0,
+        protocol         = "-1",
+        cidr_blocks      = ["0.0.0.0/0"],
+        ipv6_cidr_blocks = ["::/0"]
+      }]
 }
 
-  rds_ingress           = [
-                          {
-                            description      = "Allow HTTP from any IPv4",
-                            from_port        = 80,
-                            to_port          = 80,
-                            protocol         = "tcp",
-                            cidr_blocks      = ["0.0.0.0/0"],
-                          },
-                          {
-                            description      = "Allow connection to MYSQL",
-                            from_port        = 3306,
-                            to_port          = 3306,
-                            protocol         = "tcp",
-                            cidr_blocks      = ["0.0.0.0/0"],           
-                          }
-                          ]
-  rds_egress            = [{
-                            description      = "Allow egress to anywhere ipv4/ipv6",
-                            from_port        = 0,
-                            to_port          = 0,
-                            protocol         = "-1",
-                            cidr_blocks      = ["0.0.0.0/0"],
-                            ipv6_cidr_blocks = ["::/0"]
-                          }]
-  ec2_ingress           = [{
-                            description      = "Allow SSH from anywhere",
-                            from_port        = 22,
-                            to_port          = 22,
-                            protocol         = "tcp",
-                            cidr_blocks      = ["0.0.0.0/0"], 
-                          }]
-  ec2_egress            = [{
-                          description      = "Allow egress to anywhere ipv4/ipv6",
-                          from_port        = 0,
-                          to_port          = 0,
-                          protocol         = "-1",
-                          cidr_blocks      = ["0.0.0.0/0"],
-                          ipv6_cidr_blocks = ["::/0"]
-                          }]
 
 #   subnet_group_id       = module.networks.subnet_group_id
 #   public_subnet_id      = element(module.networks.public-subnet-ids, 0)
