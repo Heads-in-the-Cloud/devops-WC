78b2d2e82ea2cc44528f86331b4cb328b06798d5
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-03-28 17:11:22 -0700

    UTOPIA-363 configure secrets with rds terraform script

--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -1,13 +1,16 @@
 
 
 data "aws_secretsmanager_secret" "secrets" {
-  name                            = var.ssm_path
+  name = var.ssm_path
 }
 
+data "aws_secretsmanager_secret_version" "secrets" {
+  name  = var.ssm_path
+}
 
 locals {
   secrets = jsondecode(
-    data.aws_secretsmanager_secret.secrets.secret_string
+    data.aws_secretsmanager_secret_version.secrets.secret_string
   )
 }
 
