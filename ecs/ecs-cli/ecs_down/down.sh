23bd6acd219a16734184fe5e870df90a10f43679
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-03-29 23:39:28 -0700

    UTOPIA_363 aws configure region

--- ecs/ecs-cli/ecs_down/down.sh
+++ ecs/ecs-cli/ecs_down/down.sh
@@ -9,4 +9,4 @@ envsubst < "temp-docker-compose.yaml" > "docker-compose.yaml"
 
 rm temp-docker-compose.yaml
 
-ecs-cli compose --project-name ${1} --file docker-compose.yaml service rm --timeout 10
+ecs-cli compose --project-name ${1} --file docker-compose.yaml --region ${AWS_DEFAULT_REGION} service rm --timeout 10
