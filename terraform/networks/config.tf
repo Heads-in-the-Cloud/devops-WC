2ad455db7ea89243f125a33b20703c5763c4597e
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-04-20 16:07:14 -0700

    testing multi region terraform init

--- terraform/networks/config.tf
+++ terraform/networks/config.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "utopia-bucket-wc"
     key    = "terraform/${ENVIRONMENT}/${REGION_WC}/networks/terraform.tfstate"
-    region = "us-west-1"
+    region = "us-west-2"
     dynamodb_table = "WC_terraform_state"
   }
 
