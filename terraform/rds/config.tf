39ec6518ca666a415c620b5c851f67e75d5757d2
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-04-20 16:20:40 -0700

    testing multi-region for rds module

--- terraform/rds/config.tf
+++ terraform/rds/config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "utopia-bucket-wc"
-    key    = "terraform/rds/terraform.tfstate"
+    key    = "terraform/${ENVIRONMENT}/${REGION_WC}/rds/terraform.tfstate"
     region = "us-west-2"
     dynamodb_table = "WC_terraform_state"
   }
