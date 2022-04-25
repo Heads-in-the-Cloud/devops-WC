49c306c22aa5c730c509119477a3ba57b6938f86
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-04-25 16:15:06 -0700

    UTOPIA-430 using environment variables for config.tf

--- terraform/networks/config.tf
+++ terraform/networks/config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "utopia-bucket-wc"
-    key    = "terraform/${ENVIRONMENT}/${REGION}/networks/terraform.tfstate"
+    key    = "terraform/${ENVIRONMENT}/${REGION_WC}/networks/terraform.tfstate"
     region = "us-west-2"
     dynamodb_table = "WC_terraform_state"
   }
