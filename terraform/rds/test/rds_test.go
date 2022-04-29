c46e66beb932fb9984d9ce3a4e66c64281e69f39
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-04-29 13:53:40 -0700

    UTOPIA-430 testing secondary init and apply in terratest

--- terraform/rds/test/rds_test.go
+++ terraform/rds/test/rds_test.go
@@ -116,7 +116,7 @@ func TestTerraformNetworks(t *testing.T){
 
 	//Create an SSH key pair
 	KeyPairName		:= "Testing-Key-WC"
-	KeyPair 		:= aws.CreateAndImportEC2KeyPair(t, awsRegion, KeyPairName
+	KeyPair 		:= aws.CreateAndImportEC2KeyPair(t, os.Getenv("TF_VAR_region"), KeyPairName)
 
 	terraformOptionsConnectionTesting := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// The path to where our Terraform code is located
