36b6138cebd044656de678cb1947de731a3aeec3
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-04-29 13:57:47 -0700

    UTOPIA-430 testing secondary init and apply in terratest

--- terraform/rds/test/rds_test.go
+++ terraform/rds/test/rds_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/gruntwork-io/terratest/modules/aws"
-	"github.com/gruntwork-io/terratest/modules/ssh"
+	// "github.com/gruntwork-io/terratest/modules/ssh"
 )
 var deployment_passed bool
 
