9a63d1fbaf17332c9762cd8e3b4dc5bf61262164
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-04-29 13:19:55 -0700

    UTOPIA-430 testing ec2 bastion host rds

--- terraform/rds/test/rds_test.go
+++ terraform/rds/test/rds_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/tidwall/gjson"
 	"testing"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	// "github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/aws"
 )
 var deployment_passed bool
 
