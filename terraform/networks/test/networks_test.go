53529466060a790669bc429366ce97cf407b1fe6
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-04-28 19:03:12 -0700

    UTOPIA-430 adding tests using aws cli commands

--- terraform/networks/test/networks_test.go
+++ terraform/networks/test/networks_test.go
@@ -335,9 +335,9 @@ func TestTerraformNetworks(t *testing.T){
     json.Unmarshal([]byte(string(stdout)), &Routes)
 
 	for Route := range Routes {
-		fmt.Println(Route.GatewayId)
+		fmt.Println(Route)
 	}
-	Route := gjson.Get(string(stdout), "[0]")
+	// Route := gjson.Get(string(stdout), "[0]")
 	// PeeringConnectionId := gjson.Get(ActualPeeringConnection, "id")
 
     if err != nil {
