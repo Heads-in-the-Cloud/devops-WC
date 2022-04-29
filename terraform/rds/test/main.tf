4e87bcb8c1d44da29d98f10d49923d3ac1f4a34e
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-04-29 16:39:03 -0700

    UTOPIA-430 testing secondary init and apply in terratest

--- terraform/rds/test/main.tf
+++ terraform/rds/test/main.tf
@@ -27,6 +27,7 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "test_subnet" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.10.5.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
 }
 
