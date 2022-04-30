cbdc1749082818a7c301a72bdb2d54f70e72305b
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-04-29 20:25:49 -0700

    UTOPIA-430 uncommenting out terraform destroy

--- terraform/rds/test/rds_test.go
+++ terraform/rds/test/rds_test.go
@@ -182,6 +182,6 @@ func TestTerraformNetworks(t *testing.T){
 	aws.DeleteEC2KeyPair(t, KeyPair)
 
 	defer terraform.Destroy(t, terraformOptionsConnectionTesting)
-	// defer terraform.Destroy(t, terraformOptions)
+	defer terraform.Destroy(t, terraformOptions)
 
 }
