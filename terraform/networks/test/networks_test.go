0b4f5aa7bfdd68f9e86ca62ef5bb76f42ee6e343
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-05-02 15:58:39 -0700

    UTOPIA-430 refactoring

--- terraform/networks/test/networks_test.go
+++ terraform/networks/test/networks_test.go
@@ -1,13 +1,14 @@
 package networks_test
 
 import (
-	"github.com/stretchr/testify/assert" 
 	"os"
 	"os/exec"
 	"fmt"
 	"encoding/json"
-	"github.com/tidwall/gjson"
 	"testing"
+	
+	"github.com/stretchr/testify/assert" 
+	"github.com/tidwall/gjson"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/gruntwork-io/terratest/modules/aws"
 )
