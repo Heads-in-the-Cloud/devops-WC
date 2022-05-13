78c8cd77f58284b11d11d2cbe434c22f713d33b8
wjc011 <32532730+wjc011@users.noreply.github.com>
2022-05-13 14:52:37 -0700

    UTOPIA-418 adding appspec file

--- ecs/blue_green/appspec.sh
+++ ecs/blue_green/appspec.sh
@@ -2,6 +2,6 @@
 
 envsubst < appspec.sh
 
-APPSPEC=$(echo '{"version":1,"Resources":[{"TargetService":{"Type":"AWS::ECS::Service","Properties":{"TaskDefinition":"'${AWS_TASK_DEFINITION_ARN}'","LoadBalancerInfo":{"ContainerName":"${CONTAINER_NAME}","ContainerPort":$CONTAINER_PORT}}}}]}' | jq -Rs .)
+APPSPEC=$(echo '{"version":1,"Resources":[{"TargetService":{"Type":"AWS::ECS::Service","Properties":{"TaskDefinition":"'${AWS_TASK_DEFINITION_ARN}'","LoadBalancerInfo":{"ContainerName":"'${CONTAINER_NAME}'","ContainerPort":'$CONTAINER_PORT'}}}}]}' | jq -Rs .)
 
 aws deploy create-deployment --application-name wc-users-api-dev --deployment-group-name users-deployment-group --revision revisionType=AppSpecContent,appSpecContent={content=$APPSPEC}
